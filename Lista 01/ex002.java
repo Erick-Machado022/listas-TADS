acc68953fe3dd59f86aa19e91d1f0a7adcca9c66
Erick <erickmachado050@gmail.com>
2025-08-31 21:37:38 -0300

    EX 2 da lista 1 TADS

---
+++ Lista 01/ex002.java
@@ -0,0 +1,26 @@
+//O algoritmo deve ter como entrada um número real e a saída deve ser o valor atualizado com os 20%.
+
+import java.util.Scanner;
+
+public class ex002 {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        //Entrada
+        float num;
+        float atualizado;
+
+        System.out.println("Digite um número");
+        num = in.nextFloat();
+
+        //Processamento
+        atualizado = num + (num * (20.f/100.f));
+
+        //Saida
+        System.out.println(atualizado);
+
+
+
+    }
+}
