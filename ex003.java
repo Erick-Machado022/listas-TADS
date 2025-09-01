2ab4cbe4b59d2bcb14fae0ddc4a8e5254681afc3
Erick <erickmachado050@gmail.com>
2025-08-31 21:45:39 -0300

    programa calcula o quadrado do número informado pelo usuário.

---
+++ ex003.java
@@ -0,0 +1,28 @@
+// Desenvolva o algoritmo de um programa onde o usuário irá informar um número inteiro e o programa deve calcular e exibir quadrado do número informado pelo usuário.
+
+import java.util.Scanner;
+
+public class ex003 {
+
+    public static void main(String[] args) {
+       
+        Scanner in = new Scanner(System.in);
+
+        // Entrada
+        int num;
+        int quadrado;
+        System.out.println("Digite um valor inteiro: ");
+        num = in.nextInt();
+
+
+        //Processamento
+        quadrado = num * num;
+
+        //Saida
+
+        System.out.println("O quadrado de " + num + " é: " + quadrado);
+
+
+
+    }
+}
