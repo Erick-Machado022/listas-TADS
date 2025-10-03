37a5df060dd49ada88cb4abd7fd2c9fb1a142390
Erick <erickmachado050@gmail.com>
2025-10-03 16:29:29 -0300

    Lista 04 - ex03

---
+++ Lista04/nPrimo.java
@@ -0,0 +1,46 @@
+//Ler um número inteiro n. Decidir se n é um número primo e apresente o resultado.
+
+package Lista04;
+
+import java.util.Scanner;
+
+public class nPrimo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);    
+
+        int n;
+        int divisor = 2;
+        int encontrados = 0;
+
+
+        System.out.println("Informe um número inteiro: ");
+        n = in.nextInt();
+
+        if (n <= 1){
+            System.out.println(n + " não é primo");
+        }
+
+        else{
+            while (divisor <= n/2) {
+            if(n % divisor == 0){
+                System.out.println("Encontrei um divisor: " + divisor);
+                encontrados++;
+            }
+            divisor++;
+            
+        }
+
+        if (encontrados == 0) {
+            System.out.println(n + " é um número primo");
+        }
+        else{
+            System.out.println(n + " Não é primo");
+        }
+
+        }
+
+        
+
+
+    }
+}
