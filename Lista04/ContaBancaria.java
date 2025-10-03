95beac23d113eebc3a48d986724b5c2b74b62850
Erick <erickmachado050@gmail.com>
2025-10-03 16:29:36 -0300

    Lista 04 - ex04

---
+++ Lista04/ContaBancaria.java
@@ -0,0 +1,62 @@
+//Escreva um programa que apresente quatro opções: (a) consulta saldo, (b) saque e (c) depósito e (d) sair. O saldo deve iniciar em R$ 0,00. A cada saque ou depósito o valor do saldo deve ser atualizado.
+
+
+package Lista04;
+
+import java.util.Scanner;
+
+public class ContaBancaria {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+
+        float saldo = 0;
+        char opc;
+        
+
+        while (true) {
+            System.out.println("""
+                ===================
+                (a) Consultar Saldo
+                (b) Saque
+                (c) Depósito
+                (d) Sair
+                ===================
+                """);
+            System.out.println("Escolha a opção: ");
+            opc = in.next().charAt(0);
+            if (opc == 'a'){
+                System.out.println("=====SALDO=============");
+                System.out.println("R$"+saldo);
+                System.out.println("=======================");
+            }
+
+            else if(opc == 'b'){
+                float saque;
+                System.out.println("Valor a sacar: ");
+                saque = in.nextFloat();
+                if(saque > saldo){
+                    System.out.println("Não há saldo disponivel para esta transação");
+                }
+                else{
+                    saldo = saldo - saque;
+                    System.out.println("Sacando: "+ saque);
+                }
+            }
+
+            else if(opc == 'c'){
+                float deposito;
+                System.out.println("Valor a depositar: ");
+                deposito = in.nextFloat();
+                saldo += deposito;
+                System.out.println("Acrescentando " + deposito + " em seu saldo.");
+            }
+
+            else if (opc == 'd') {
+                System.out.println("Finalizando programa");
+                System.out.println("<<<VOLTE SEMPRE>>>");
+                break;
+            }
+        }
+        System.out.println("Programa finalizado.");
+    }
+}
