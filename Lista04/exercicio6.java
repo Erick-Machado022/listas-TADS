28a22d20c3d246e48852e6477f6633f9431a3fcf
Erick <erickmachado050@gmail.com>
2025-10-08 22:52:24 -0300

    Lista 4 ex6

---
+++ Lista04/exercicio6.java
@@ -0,0 +1,27 @@
+/*
+Escreva um algoritmo para ler um valor entre 1 (inclusive) e 10 (inclusive). Se o valor
+lido não estiver entre 1 (inclusive) e 10 (inclusive), deve ser lido um novo valor. Após
+a leitura do valor, escrever o valor lido na tela.
+ */
+
+package Lista04;
+
+import java.util.Scanner;
+
+public class exercicio6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = 0;
+        while(true){
+            System.out.println("Informe um número inteiro: ");
+            num = in.nextInt();
+            if(num >=1 && num <= 10){
+                break;
+            }
+            else{
+                System.out.println("Informe novamente. ");
+            }
+        }
+        System.out.println(num);
+    }
+}
