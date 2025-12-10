1e73b66658568ad5b4d9b1a631127077dfd5efd6
Erick <erickmachado050@gmail.com>
2025-12-09 21:31:49 -0300

    Lista 11 ex9

---
+++ Lista 11/ex9.java
@@ -0,0 +1,33 @@
+public class ex9 {
+    public static void main(String[] args) {
+        int [] [] matriz = {
+            {2,1,3,4},
+            {1,2,3,4},
+            {1,2,2,4},
+            {1,2,3,2},
+        };
+
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("==============");
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if(i==j){
+                    soma= soma + matriz[i][j];
+                }
+            }
+        }
+
+
+
+        System.out.println(soma);
+    }   
+}
